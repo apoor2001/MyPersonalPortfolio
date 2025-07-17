9f495be50c7daea5c92225342885511c46eb1cd7
APOORVA TRIPATHI <apoorvatripathi@APOORVAs-MacBook-Air.local>
2025-07-17 18:34:41 +0530

    default theme changed to dark

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,16 +3,19 @@ import { useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
 
 export const ThemeToggle = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(true); // default to dark
 
   useEffect(() => {
     const storedTheme = localStorage.getItem("theme");
-    if (storedTheme === "dark") {
+
+    if (storedTheme === "light") {
+      setIsDarkMode(false);
+      document.documentElement.classList.remove("dark");
+    } else {
+      // default to dark if no value is found or if "dark" is stored
       setIsDarkMode(true);
       document.documentElement.classList.add("dark");
-    } else {
-      localStorage.setItem("theme", "light");
-      setIsDarkMode(false);
+      localStorage.setItem("theme", "dark");
     }
   }, []);
 
@@ -29,18 +32,18 @@ export const ThemeToggle = () => {
   };
 
   return (
-    <button
-      onClick={toggleTheme}
-      className={cn(
-        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
-        "focus:outlin-hidden"
-      )}
-    >
-      {isDarkMode ? (
-        <Sun className="h-6 w-6 text-yellow-300" />
-      ) : (
-        <Moon className="h-6 w-6 text-blue-900" />
-      )}
-    </button>
+      <button
+          onClick={toggleTheme}
+          className={cn(
+              "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+              "focus:outline-none"
+          )}
+      >
+        {isDarkMode ? (
+            <Sun className="h-6 w-6 text-yellow-300" />
+        ) : (
+            <Moon className="h-6 w-6 text-blue-900" />
+        )}
+      </button>
   );
 };
